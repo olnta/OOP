6b4502a823b7a8ca12821f9512b2915a40b6c60f
olnta.b1 <olnta.b1@schule.bremen.de>
2022-11-28 11:30:06 +0100

    Gameplay

--- Arbeit/src/Gameplay.java
+++ Arbeit/src/Gameplay.java
@@ -2,17 +2,25 @@ import java.util.ArrayList;
 
 public class Gameplay {
 
-    private static final int spielzeit = 90;
-    private static final int max_nachspielzeit = 5;
-    private static final int max_dauer_bis_aktion = 10;
+    private static final int SPIELZEIT = 90;
+    private static final int MAX_NACHSPIELZEIT = 5;
+    private static final int MAX_DAUER_BIS_AKTION = 10;
 
     private static int ermittelMannschaftsWert(Mannschaft mannschaft) {
-        int m = mannschaft.getMotivation();
-        int s = mannschaft.getStaerke();
-        int t = mannschaft.getTrainer().getErfahrung();
 
-        int mannsch = t * s * m;
-        return mannsch;
+
+        /** int m = mannschaft.getMotivation();
+         *  int s = mannschaft.getStaerke();
+         *  int t = mannschaft.getTrainer().getErfahrung();
+         *  int mannsch = t * s * m;
+         */
+
+        int mannschaftswert = mannschaft.getMotivation() * mannschaft.getStaerke() * mannschaft.getTrainer().getErfahrung();
+        if (mannschaftswert < 1){
+            mannschaftswert = 1;
+        }
+
+        return mannschaftswert;
     }
 
     private static boolean erzieltTor(Spieler schuetze, Torwart torwart){
