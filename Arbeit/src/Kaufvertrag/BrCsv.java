03b00a0df2db7ab94e829d65b3763d1921c686e6
olnta.b1 <olnta.b1@schule.bremen.de>
2023-02-28 13:20:16 +0100

    BufferedReader get info from csv to the console and sout it.

--- Arbeit/src/Kaufvertrag/BrCsv.java
+++ Arbeit/src/Kaufvertrag/BrCsv.java
@@ -3,14 +3,18 @@ package Kaufvertrag;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class BrCsv {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("test.csv"));
 
-        String csv;
+        String csv = "";
+        while(null!=br.readLine()){
+
+        }
 
 
     }
