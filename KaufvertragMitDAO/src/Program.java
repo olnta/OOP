1a8072f0c8016e094e2408c2d3ea17321950f6b1
olnta.b1 <olnta.b1@schule.bremen.de>
2023-08-31 15:07:20 +0200

    Kaufvertrag Anfang

--- KaufvertragMitDAO/src/Program.java
+++ KaufvertragMitDAO/src/Program.java
@@ -1,10 +1,16 @@
 import Dao.VertragspartnerDao;
+import businessObjects.Vertragspartner;
 
 public class Program {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		//Deklaration und Initialisierung einer Instanz von VertragspartnerDao
 		VertragspartnerDao vertragspartnerDao = new VertragspartnerDao();
+
+		//read(String nr)-Methode aus VertragsparnterDao aufrufen und
+		//das Ergebnis in ein Objekt des Typs Vertragspartner schreiben.
+		Vertragspartner vertragspartner = vertragspartnerDao.read("0123456789");
+		System.out.println(vertragspartner);
 
 	}
 
