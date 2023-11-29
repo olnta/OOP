73a5958ee1118310b4e36b7022281cc485e9637b
olnta.b1 <olnta.b1@schule.bremen.de>
2023-11-29 11:28:56 +0100

    GUI

---
+++ GUI/src/Startbeispiel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Startbeispiel {
+    private JLabel Vorname;
+    private JTextField textField1;
+    private JLabel Nachname;
+    private JTextField textField2;
+    private JButton Text;
+    private JButton button1;
+}
